8c9441143bbdf49594de35fbdc50e47175b8af92
Scott Wolchok <swolchok@umich.edu>
2009-06-29 01:05:37 -0400

    environment vars for build for linux

---
+++ linux_pyds.sh
@@ -0,0 +1,24 @@
+export PATH=$DEVKITARM/bin:$PATH
+export INCLUDE=$DEVKITARM/arm-eabi/include
+export LIB=$DEVKITARM/arm-eabi/lib
+#
+export CC=$DEVKITARM/bin/arm-eabi-gcc
+#
+# rem ndslib
+#
+# SET NDSLIB_INCLUDE=c:\devkitPro\libnds\include
+# SET NDSLIB_LIB=c:\devkitPro\libnds\lib
+#
+#
+# rem stackless
+#
+# set BASECFLAGS=-ffast-math -mthumb -mthumb-interwork -DARM9 -DNDS
+# set CFLAGS=-ffast-math -mthumb -mthumb-interwork -DARM9 -DNDS
+# set LDFLAGS=-specs=ds_arm9.specs -g -mthumb -mthumb-interwork
+#
+# set CC_FOR_BUILD=gcc
+#
+# set PYTHON_FOR_BUILD=/c/Python24/python
+#
+#
+# c:\devkitPro\msys\msys.bat
